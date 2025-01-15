c0c8ad6cdb6aa235d60d8c7a68128e8714ac04b7
pankajganhotra <pankajganhotra021@gmail.com>
2025-01-15 18:45:44 +0530

    delimeter change - also support new line (\n)

--- add.js
+++ add.js
@@ -1,7 +1,7 @@
 function add(numbers) {
   if (!numbers) return 0;
 
-  const numberArray = numbers.split(",")
+  const numberArray = numbers.split(/,|\n/).map(num => parseFloat(num.trim()));
 
   return numberArray.reduce((sum, num) => sum + +num, 0);
 }
--- add.test.js
+++ add.test.js
@@ -16,4 +16,10 @@ describe("add function", () => {
     expect(add("10,20,30,40,50,60")).toBe(210);
     expect(add("5,15,20,25,30,25,40,45,50")).toBe(255);
   });
+
+  test('should handle newlines between numbers', () => {
+    expect(add('1\n2,3')).toBe(6);
+    expect(add('10\n20\n30')).toBe(60);
+    expect(add('1\n5,  10\n15')).toBe(31);
+  });
 });
